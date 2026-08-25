5425885a4dbc5595ee67effb7e2de63b9604f8b0
janice-shnoor <janice@shnoor.com>
2026-08-25 16:27:46 +0530

    Initial Project Setup: Frontend and Backend

---
+++ backend/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json()); // for parsing JSON bodies
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
